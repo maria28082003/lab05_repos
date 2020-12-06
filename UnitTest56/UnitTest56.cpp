b2fb2e1f9f7bf15e909a53890b8a41ff8a194a03
maria28082003 <72791150+maria28082003@users.noreply.github.com>
2020-12-06 20:09:20 +0200

    Add files via upload

---
+++ UnitTest56/UnitTest56.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication5.6/ConsoleApplication5.6.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest56
+{
+	TEST_CLASS(UnitTest56)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
